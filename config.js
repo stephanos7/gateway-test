d6b889ca96ef36678a3f16fa4ed51ebe1b64f795
Stephanos <ste.the@outlook.com>
2020-04-04 18:27:02 +0300

    push with default heroku ss

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  // ssl: isProduction,
 })
 
 module.exports = {pool};
