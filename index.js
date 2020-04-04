2b73497d6c2eb2e5c96e9ad2ead6c400e68251cc
Stephanos <ste.the@outlook.com>
2020-04-04 13:26:33 +0300

    Add parsing middleware

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var express = require("express");
+
+// define middleware
+var bodyParser = require("body-parser");
+
+// instantiate express server
 var app = express();
+
+// use middleware
+  // use body-parser to return middleware that parse json & url-encoded bodies
+  // the extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true). The
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
 
 app.get("/", (req, res, next) => {
   res.json(["hello from the index service"]);
